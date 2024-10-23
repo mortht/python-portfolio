fda9946eba18acdbb3a5bba37bfd4a0e47f00fba
morthth <thiemo.morth@arhs-developments.com>
2024-10-23 15:16:46 +0200

    Add Contact Me web form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Contact Me")
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    submit_form = st.form_submit_button("Submit")
+    if submit_form:
+        print("Pressed")
