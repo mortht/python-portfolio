289d671e4d6c38b4523d407ef97756020477ff89
morthth <thiemo.morth@arhs-developments.com>
2024-10-23 13:46:56 +0200

    Add description on the new columns

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ with col2:
     Trier in Germany. ...
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
