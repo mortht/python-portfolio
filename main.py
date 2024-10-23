0f12b9310740c8a68e457bc89009ec554d1f5177
morthth <thiemo.morth@arhs-developments.com>
2024-10-23 13:14:52 +0200

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/me.jpeg")
+
+with col2:
+    st.title("Thiemo Morth")
+    content = """
+    Hi, I am Thiemo! I am a Software Architect, Java and Python programmer, a father and Mountainbike rider.
+    I graduated in 2008 with a Master of Science in Computer Sciences from the University of Applied Science 
+    Trier in Germany. ...
+    """
+    st.info(content)
