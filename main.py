28f43d1e16c172f11550139cab4fc0e9b135cc4c
morthth <thiemo.morth@arhs-developments.com>
2024-10-23 14:12:13 +0200

    Render description, images and source code links. Add space between the 2 columns

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/me.jpeg", width=600)
+    st.image("images/me.jpeg", width=400)
 
 with col2:
     st.title("Thiemo Morth")
@@ -22,14 +22,20 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=',')
 
 with col3:
     for idx, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for idx, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
