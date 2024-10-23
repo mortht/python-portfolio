903de323f8188c6ff60241b2d09fdb92f46e05c5
morthth <thiemo.morth@arhs-developments.com>
2024-10-23 13:29:33 +0200

    Update image with

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/me.jpeg")
+    st.image("images/me.jpeg", width=600)
 
 with col2:
     st.title("Thiemo Morth")
